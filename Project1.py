783fce1273baad7275c6a2c83b958c37e762539b
shivansh gupta <shivanshgupta7201@gmail.com>
2023-09-23 12:22:17 +0530

    Text to Speech

--- Project1.py
+++ Project1.py
@@ -1,7 +1,7 @@
 import pyttsx3
 
 engine = pyttsx3.init()
-engine.say("Hey gus,this is a text to Speech in PYTHON!!!")
+engine.say("Hey gus,this is a text to   oh my goodness I am a robot")
 engine.runAndWait()
 engine.say("Firstly!!!   I wanna ask one Question which is ?" + "How are you?")
 engine.runAndWait()
