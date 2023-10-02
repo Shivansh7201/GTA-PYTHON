89f21390c988b766cc320a4aaa6050cd7bd5fc74
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-10-02 22:22:47 +0530

    Add files via upload

--- Set data type.py
+++ Set data type.py
@@ -1,6 +1,26 @@
 #*************************{" Set Data Type "}******************
 
-# NOTE :- If we want to represent a roup of values without duplicates where order is not important,
+# NOTE :- If we want to represent a group of values without duplicates where order is not important,
 #         then we should go for SET DATA TYPE.
 
-#---> 1)
+#--->  || 1) Insertion order is not preserved. ||
+#--->  || 2) Duplicates are not allowed.  ||
+#--->  || 3) Indexing and slicing are not applicable  ||
+#--->  || 4) Heterogeneous objects are allowed.  ||
+#--->  || 5) Growable in nature.   ||
+
+s={100,0,10,200,101,'durga'}
+print(s) # {0, 100, 101, 'durga', 10, 200}
+print()
+print(type(s)) # <class 'set'>
+print()
+#print(s[0]) # TypeError: 'set' object does not support indexing
+
+# %%%%%%%%%% NOTE "SET is growable in nature, based on our requirement we can increase OR " %%%%%%%%%%%%%
+# %%%%%%%%%% "decrease the size. There is no restriction. " %%%%%%%%%%%%%
+
+s.add(742)
+print(s) # {0, 100, 101, 'durga', 10, 200, 742}
+print()
+s.remove(200)
+print(s) # {0, 100, 101, 'durga', 10, 742}
