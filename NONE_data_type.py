915f709e5d89f793f3da98bdd174bfab90495d8b
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-10-02 23:19:38 +0530

    checkit

---
+++ NONE_data_type.py
@@ -0,0 +1,17 @@
+# None means nothing,or No value ASSOCIATED.
+
+# IF the value is not avialable then to handle this situation we use None data type.
+
+# None is a keyword in python.
+
+# None is not equal to 0 or False.
+
+# None is equal to None.
+
+# it's is something Null values.
+
+# EXample:=-
+
+def m1():
+    a=10
+print(m1()) #None
