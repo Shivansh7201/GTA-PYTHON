802dff5f571f12c35ceafe69b3727b1760919733
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-10-08 15:24:29 +0530

    Add files via upload

---
+++ Escape_char.py
@@ -0,0 +1,30 @@
+#(NOTE):- In string Literals we can use escape characters to associate a special meaning.
+
+s="shivansh\nGupta" #(NEW LINE--> "\n")
+print(s)
+print()
+
+d="&emicolon\tMeraNaam" #(Horizontal Tab--> "\t")
+print(d)
+print()
+
+s1="Hello\rKaiseho" #(Carriage Return-->"\r")
+print(s1)
+print()
+
+s2="shiva\bgupta"#(backspace-->"\b")
+print(s2)
+print()
+
+v="Aur\fkitthhe" #(Form Feed-->"\f")
+print(v)
+print()
+
+c="Lamborghini\vaaechalyejaneoo" #(Vertical Tab)
+print(c)
+print()
+
+print("Sanno\\bhi\'chotta de\"doo") 
+#( for printing (')-->\')
+#(for printing(")-->\")
+# (\\)-->BAck Slash
