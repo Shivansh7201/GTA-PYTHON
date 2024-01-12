aa93485a621187d5de83b04c34fb492b2dc16879
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-12 22:40:17 +0530

    Create Complete the script.py

---
+++ Complete the script.py
@@ -0,0 +1,9 @@
+"""Complete the script by filling in the missing parts. The function receives a name, then returns a greeting based on whether or not that name is "Taylor"."""
+def greeting(name):
+  if name is "taylor":
+    return "Welcome back taylor"
+  else:
+    return "hello there's" + name
+print(greeting("Traylor"))
+print(greeting("John"))
+       
