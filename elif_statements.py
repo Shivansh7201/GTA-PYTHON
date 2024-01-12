c2c12366282a07f817f7fc9b47beee5137d72858
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-12 22:31:44 +0530

    Create elif_statements.py

---
+++ elif_statements.py
@@ -0,0 +1,9 @@
+def hint_username(username):
+    if len(username) < 3:
+        print("Invalid username. Must be at least 3 characters long")
+    else:
+        if len(username) > 15:
+            print("Invalid username. Must be at most 15 characters long")
+        else:
+            print("Valid username")
+hint_username("shi")            
