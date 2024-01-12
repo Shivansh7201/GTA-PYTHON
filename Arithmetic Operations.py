b3c6b3b81cb5e09fda98389503ae8c9a8fdb4df6
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-12 22:35:12 +0530

    Create Arithmetic Operations.py

---
+++ Arithmetic Operations.py
@@ -0,0 +1,18 @@
+# This function rounds a variable number up to the nearest 10x value
+def round_up(number):
+  x = 10
+# The floor division operator will calculate the integer value of
+# "number" divided by x: 35 // 10 will return the integer 3.
+  whole_number = number // x
+# The modulo operator will calculate the remainder value of "number"
+# divided by x: 35 % 10 will return the remainder value 5.
+  remainder = number % x
+# If the remainder is greater than 0: 
+  if remainder >= 5: 
+# Return x multiplied by the (whole_number+1) to round up
+    return x*(whole_number+1)
+# Else, return x multiplied by the whole_number to round down
+  return x*whole_number
+ 
+# Calls the function with the parameter value of 35.
+print(round_up(35)) # Should print 40
