192aee5d42c5b38d2150bbfbcda10f0a2579fa56
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2024-01-13 20:29:02 +0530

    Add files via upload

---
+++ Print_Without_print.py
@@ -0,0 +1,11 @@
+"""
+   Print without newline Using Python sys module
+   To use the sys module, first, import the module sys using the import keyword. 
+   Then, make use of the stdout.write() method available inside the sys module, 
+   to print your strings. It only works with string If you pass a number or a list, 
+   you will get a TypeError.
+"""
+import sys
+
+sys.stdout.write("GeeksforGeeks ")
+sys.stdout.write("is best website for coding!")
